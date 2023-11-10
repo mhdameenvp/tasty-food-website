f41b9f7a817d8e0f6f57926c6188d493ab449409
MUHAMMED AMEEN VP <mhdameenvp1126@gmail.com>
2023-11-10 10:49:42 +0530

    intial

---
+++ javascript.js
@@ -0,0 +1,26 @@
+
+document.getElementById('wb1').addEventListener('click', function() {
+    alert('THANK YOU FOR GIVING FIVE STAR REVIEW') ;
+  });
+  
+document.getElementById('wb2').addEventListener('click', function() {
+    alert('THANK YOU FOR GIVING FOUR STAR REVIEW');
+  });
+  
+
+  document.getElementById('wb3').addEventListener('click', function() {
+    alert('THANK YOU FOR GIVING THEREE STAR REVIEW');
+  });
+  
+
+  document.getElementById('wb4').addEventListener('click', function() {
+    alert('THANK YOU FOR GIVING TWO STAR REVIEW');
+  });
+  
+
+  document.getElementById('wb5').addEventListener('click', function() {
+    alert('THANK YOU FOR GIVING ONE STAR REVIEW');
+  });
+  
+
+  
